8ec07bbeaadf511ffc2198a954997509817596c5
nflFauzan <naufalmnf1692006@gmail.com>
2025-11-26 14:06:24 +0700

    setup project

---
+++ src/main/java/id/ac/tazkia/minibank/AplikasiMiniBankApplication.java
@@ -0,0 +1,13 @@
+package id.ac.tazkia.minibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiMiniBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiMiniBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/tazkia/minibank/AplikasiMiniBankApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.tazkia.minibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiMiniBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
