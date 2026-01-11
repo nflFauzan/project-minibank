c0b725088d4c5c8e328fbfdb9c17a5da2bd90fb3
nflFauzan <naufalmnf1692006gmail.com>
2026-01-12 01:20:07 +0700

    Produk di Supervisor & warna merah transfer ke

--- src/main/java/id/ac/tazkia/minibank/controller/SupervisorController.java
+++ src/main/java/id/ac/tazkia/minibank/controller/SupervisorController.java
@@ -1,16 +1,24 @@
 package id.ac.tazkia.minibank.controller;
 
 import id.ac.tazkia.minibank.entity.NasabahStatus;
+import id.ac.tazkia.minibank.entity.ProdukTabungan;
+import id.ac.tazkia.minibank.service.DashboardService;
 import id.ac.tazkia.minibank.repository.NasabahRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import id.ac.tazkia.minibank.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/supervisor")
 public class SupervisorController {
 
+@Autowired
+    private DashboardService dashboardService;
     private final NasabahRepository nasabahRepository;
 
     public SupervisorController(NasabahRepository nasabahRepository) {
@@ -19,8 +27,10 @@ public class SupervisorController {
 
     @GetMapping("/dashboard")
     public String supervisorDashboard(Model model) {
+
         long pendingCount = nasabahRepository.countByStatus(NasabahStatus.INACTIVE);
         model.addAttribute("pendingCount", pendingCount);
+  model.addAttribute("products", dashboardService.getActiveProdukTabungan());
         return "supervisor/dashboard";
     }
 }
